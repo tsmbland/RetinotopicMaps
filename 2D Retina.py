10ccf139833ac7a1e17618c956f33c6971d20210
tsmbland <tb503@cam.ac.uk>
2016-12-16 11:57:19 +0000

    Boundary conditions with zeros padding for 2D retina

--- 2D Retina.py
+++ 2D Retina.py
@@ -41,10 +41,13 @@ Iterations = 20  # number of weight iterations
 # nR = NR  # present number of retinal cells (pre-surgery)
 # nT = NT  # present number of tectal cells (pre-surgery)
 
+rnm = np.zeros([NRdim1 + 2, NRdim2 + 2])  # retinal neuron map (fixed)
+rnm[1:-1, 1:-1] = 1
+
 # Wpt = np.zeros([nT, nR])  # synaptic strength between a presynaptic cell and a postsynaptic cell
-Qpm = np.zeros([M, NRdim1, NRdim2])  # presence of marker sources along retina
+Qpm = np.zeros([M, NRdim1 + 2, NRdim2 + 2])  # presence of marker sources along retina
 # Qtm = np.zeros([nT, M])  # axonal flow of molecules into postsymaptic cells
-Cpm = np.zeros([M, NRdim1, NRdim2])  # concentration of a molecule in a presynaptic cell
+Cpm = np.zeros([M, NRdim1 + 2, NRdim2 + 2])  # concentration of a molecule in a presynaptic cell
 # Ctm = np.zeros([nT, M])  # concentration of a molecule in a postsynaptic cell
 # normalisedCpm = np.zeros([nR, M])  # normalised (by marker conc.) marker concentration  in a presynaptic cell
 # normalisedCtm = np.zeros([nT, M])  # normalised (by marker conc.) marker concentration in a postsynaptic cell
@@ -55,56 +58,48 @@ Cpm = np.zeros([M, NRdim1, NRdim2])  # concentration of a molecule in a presynap
 
 # MARKER LOCATIONS
 
-Qpm[0, 0, 0] = Q
-Qpm[1, 0, NRdim2 - 1] = Q
-Qpm[2, NRdim1 - 1, 0] = Q
-Qpm[3, NRdim1 - 1, NRdim2 - 1] = Q
+Qpm[0, 1, 1] = Q
+Qpm[1, 1, NRdim2] = Q
+Qpm[2, NRdim1, 1] = Q
+Qpm[3, NRdim1, NRdim2] = Q
 Qpm[4, NRdim1 / 2, NRdim2 / 2] = Q
 
 
 # PRESYNAPTIC CONCENTRATIONS
 
 def conc_change(concmatrix, layer):
+
+    # Matrix size
+    Ndim1 = len(concmatrix[0, :, 0]) - 2
+    Ndim2 = len(concmatrix[0, 0, :]) - 2
+
+    # Neuron map
+    nm = np.zeros([Ndim1 + 2, Ndim2 + 2])
+    nm[1:-1, 1:-1] = 1
+
+    # Neighbour Count
+    nc = np.zeros([Ndim1 + 2, Ndim2 + 2])
+    for dim1 in range(1, Ndim1 + 1):
+        for dim2 in range(1, Ndim2 + 1):
+            nc[dim1, dim2] = nm[dim1 + 1, dim2] + nm[dim1 - 1, dim2] + nm[dim1, dim2 + 1] + nm[dim1, dim2 - 1]
+
+    # Qmatrix
     if layer == 'presynaptic':
         Qmatrix = Qpm
     elif layer == 'tectal':
         Qmatrix = Qtm
 
-    concchange = np.zeros([M, NRdim1, NRdim2])
+    # Conc change
+    concchange = np.zeros([M, Ndim1 + 2, Ndim2 + 2])
     for m in range(M):
-        concchange[m, 0, 0] = (
-        -a * concmatrix[m, 0, 0] + d * (concmatrix[m, 1, 0] + concmatrix[m, 0, 1] - 2 * concmatrix[m, 0, 0]) + Qmatrix[
-            m, 0, 0])
-        concchange[m, NRdim1 - 1, NRdim2 - 1] = (-a * concmatrix[m, NRdim1 - 1, NRdim2 - 2] + d * (
-        concmatrix[m, NRdim1 - 2, NRdim2 - 1] + concmatrix[m, NRdim1 - 1, NRdim2 - 2] - 2 * concmatrix[
-            m, NRdim1 - 1, NRdim2 - 1]) + Qmatrix[m, NRdim1 - 1, NRdim2 - 1])
-        concchange[m, 0, NRdim2 - 1] = (-a * concmatrix[m, 0, NRdim2 - 1] + d * (
-        concmatrix[m, 1, NRdim2 - 1] + concmatrix[m, 0, NRdim2 - 2] - 2 * concmatrix[m, 0, NRdim2 - 1]) + Qmatrix[
-                                            m, 0, NRdim2 - 1])
-        concchange[m, NRdim1 - 1, 0] = (-a * concmatrix[m, NRdim1 - 1, 0] + d * (
-        concmatrix[m, NRdim1 - 2, 0] + concmatrix[m, NRdim1 - 1, 1] - 2 * concmatrix[m, NRdim1 - 1, 0]) + Qmatrix[
-                                            m, NRdim1 - 1, 0])
-        for dim1 in range(1, NRdim1 - 1):
-            concchange[m, dim1, 0] = (-a * concmatrix[m, dim1, 0] + d * (
-            concmatrix[m, dim1 + 1, 0] + concmatrix[m, dim1 - 1, 0] + concmatrix[m, dim1, 1] - 3 * concmatrix[
-                m, dim1, 0]) + Qmatrix[m, dim1, 0])
-            concchange[m, dim1, NRdim2 - 1] = (-a * concmatrix[m, dim1, NRdim2 - 1] + d * (
-            concmatrix[m, dim1 + 1, NRdim2 - 1] + concmatrix[m, dim1 - 1, NRdim2 - 1] + concmatrix[
-                m, dim1, NRdim2 - 2] - 3 * concmatrix[m, dim1, NRdim2 - 1]) + Qmatrix[m, dim1, NRdim2 - 1])
-        for dim2 in range(1, NRdim2 - 1):
-            concchange[m, 0, dim2] = (-a * concmatrix[m, 0, dim2] + d * (
-            concmatrix[m, 0, dim2 + 1] + concmatrix[m, 0, dim2 - 1] + concmatrix[m, 1, dim2] - 3 * concmatrix[
-                m, 0, dim2]) + Qmatrix[m, 0, dim2])
-            concchange[m, NRdim1 - 1, dim2] = (-a * concmatrix[m, NRdim1 - 1, dim2] + d * (
-            concmatrix[m, NRdim1 - 1, dim2 + 1] + concmatrix[m, NRdim1 - 1, dim2 - 1] + concmatrix[
-                m, NRdim1 - 2, dim2] - 3 * concmatrix[m, NRdim1 - 1, dim2]) + Qmatrix[m, NRdim1 - 1, dim2])
-        for dim1 in range(1, NRdim1 - 1):
-            for dim2 in range(1, NRdim2 - 1):
+        for dim1 in range(1, Ndim1 + 1):
+            for dim2 in range(1, Ndim2 + 1):
                 concchange[m, dim1, dim2] = (-a * concmatrix[m, dim1, dim2] + d * (
-                concmatrix[m, dim1, dim2 + 1] + concmatrix[m, dim1, dim2 - 1] + concmatrix[m, dim1 + 1, dim2] +
-                concmatrix[m, dim1 - 1, dim2] - 4 * concmatrix[m, dim1, dim2]) + Qmatrix[m, dim1, dim2])
+                    concmatrix[m, dim1, dim2 + 1] + concmatrix[m, dim1, dim2 - 1] + concmatrix[m, dim1 + 1, dim2] +
+                    concmatrix[m, dim1 - 1, dim2] - nc[dim1, dim2] * concmatrix[m, dim1, dim2]) + Qmatrix[m, dim1, dim2])
 
     return concchange
+
 
 
 averagemarkerchange = 1
@@ -119,7 +114,7 @@ plt.rcParams.update(params)
 
 for m in range(1, M + 1):
     plt.subplot(3, 2, m)
-    plt.pcolormesh(Cpm[m - 1, :, :])
+    plt.pcolormesh(Cpm[m - 1, 1:NRdim1+1, 1:NRdim2+1])
     plt.ylabel('Dimension 1')
     plt.xlabel('Dimension 2')
 
