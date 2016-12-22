0685ff987875e5963225d019918887405159bb9e
tsmbland <tb503@cam.ac.uk>
2016-12-22 17:44:45 +0000

    Initial connections and normalisation working

--- 2D Retina.py
+++ 2D Retina.py
@@ -9,10 +9,10 @@ start = time.time()
 #################### PARAMETERS #####################
 
 # General
-NRdim1 = 20  # initial number of retinal cells
-NRdim2 = 20
-
-# NT = 80  # initial number of tectal cells
+NRdim1 = 10  # initial number of retinal cells
+NRdim2 = 10
+NTdim1 = 10  # initial number of tectal cells
+NTdim2 = 10
 M = 5  # number of markers
 
 # Presynaptic concentrations
@@ -23,8 +23,8 @@ Q = 100  # release of markers from source
 stab = 0.1  # retinal stability threshold
 
 # Establishment of initial contacts
-n0 = 8  # number of initial random contact
-NL = 60  # sets initial bias
+n0 = 10  # number of initial random contact
+NL = 7  # sets initial bias
 
 # Tectal concentrations
 deltat = 0.1  # time step
@@ -38,21 +38,23 @@ elim = 0.005  # elimination threshold
 Iterations = 20  # number of weight iterations
 
 ################### VARIABLES ###################
-# nR = NR  # present number of retinal cells (pre-surgery)
-# nT = NT  # present number of tectal cells (pre-surgery)
+nRdim1 = NRdim1  # present number of retinal cells (pre-surgery)
+nRdim2 = NRdim2
+nTdim1 = NTdim1  # present number of tectal cells (pre-surgery)
+nTdim2 = NTdim2
 
 rnm = np.zeros([NRdim1 + 2, NRdim2 + 2])  # retinal neuron map (fixed)
 rnm[1:-1, 1:-1] = 1
 
-# Wpt = np.zeros([nT, nR])  # synaptic strength between a presynaptic cell and a postsynaptic cell
+Wpt = np.zeros([NTdim1+2, NTdim2+2, NRdim1+2, NRdim2+2])  # synaptic strength between a presynaptic cell and a postsynaptic cell
 Qpm = np.zeros([M, NRdim1 + 2, NRdim2 + 2])  # presence of marker sources along retina
-# Qtm = np.zeros([nT, M])  # axonal flow of molecules into postsymaptic cells
+Qtm = np.zeros([M, NTdim1 + 2, NTdim2 + 2])  # axonal flow of molecules into postsymaptic cells
 Cpm = np.zeros([M, NRdim1 + 2, NRdim2 + 2])  # concentration of a molecule in a presynaptic cell
-# Ctm = np.zeros([nT, M])  # concentration of a molecule in a postsynaptic cell
-# normalisedCpm = np.zeros([nR, M])  # normalised (by marker conc.) marker concentration  in a presynaptic cell
-# normalisedCtm = np.zeros([nT, M])  # normalised (by marker conc.) marker concentration in a postsynaptic cell
-
-
+Ctm = np.zeros([M, NTdim1 + 2, NTdim2 + 2])  # concentration of a molecule in a postsynaptic cell
+normalisedCpm = np.zeros(
+    [M, NRdim1 + 2, NRdim2 + 2])  # normalised (by marker conc.) marker concentration  in a presynaptic cell
+normalisedCtm = np.zeros(
+    [M, NTdim1 + 2, NTdim2 + 2])  # normalised (by marker conc.) marker concentration in a postsynaptic cell
 
 ################## RETINA #####################
 
@@ -68,19 +70,18 @@ Qpm[4, NRdim1 / 2, NRdim2 / 2] = Q
 # PRESYNAPTIC CONCENTRATIONS
 
 def conc_change(concmatrix, layer):
-
     # Matrix size
-    Ndim1 = len(concmatrix[0, :, 0]) - 2
-    Ndim2 = len(concmatrix[0, 0, :]) - 2
+    ndim1 = len(concmatrix[0, :, 0]) - 2
+    ndim2 = len(concmatrix[0, 0, :]) - 2
 
     # Neuron map
-    nm = np.zeros([Ndim1 + 2, Ndim2 + 2])
+    nm = np.zeros([ndim1 + 2, ndim2 + 2])
     nm[1:-1, 1:-1] = 1
 
     # Neighbour Count
-    nc = np.zeros([Ndim1 + 2, Ndim2 + 2])
-    for dim1 in range(1, Ndim1 + 1):
-        for dim2 in range(1, Ndim2 + 1):
+    nc = np.zeros([ndim1 + 2, ndim2 + 2])
+    for dim1 in range(1, ndim1 + 1):
+        for dim2 in range(1, ndim2 + 1):
             nc[dim1, dim2] = nm[dim1 + 1, dim2] + nm[dim1 - 1, dim2] + nm[dim1, dim2 + 1] + nm[dim1, dim2 - 1]
 
     # Qmatrix
@@ -90,16 +91,16 @@ def conc_change(concmatrix, layer):
         Qmatrix = Qtm
 
     # Conc change
-    concchange = np.zeros([M, Ndim1 + 2, Ndim2 + 2])
+    concchange = np.zeros([M, ndim1 + 2, ndim2 + 2])
     for m in range(M):
-        for dim1 in range(1, Ndim1 + 1):
-            for dim2 in range(1, Ndim2 + 1):
+        for dim1 in range(1, ndim1 + 1):
+            for dim2 in range(1, ndim2 + 1):
                 concchange[m, dim1, dim2] = (-a * concmatrix[m, dim1, dim2] + d * (
                     concmatrix[m, dim1, dim2 + 1] + concmatrix[m, dim1, dim2 - 1] + concmatrix[m, dim1 + 1, dim2] +
-                    concmatrix[m, dim1 - 1, dim2] - nc[dim1, dim2] * concmatrix[m, dim1, dim2]) + Qmatrix[m, dim1, dim2])
+                    concmatrix[m, dim1 - 1, dim2] - nc[dim1, dim2] * concmatrix[m, dim1, dim2]) + Qmatrix[
+                                                 m, dim1, dim2])
 
     return concchange
-
 
 
 averagemarkerchange = 1
@@ -108,13 +109,93 @@ while averagemarkerchange > stab:
     averagemarkerchange = sum(sum(sum(deltaconc))) / sum(sum(sum(Cpm))) * 100
     Cpm += (deltaconc * deltat)
 
+
+# NORMALISED PRESYNAPTIC CONCENTRATIONS
+
+def normalise(concmatrix):
+    # Matrix size
+    ndim1 = len(concmatrix[0, :, 0]) - 2
+    ndim2 = len(concmatrix[0, 0, :]) - 2
+
+    # Marker sum
+    markersum = np.zeros([ndim1 + 2, ndim2 + 2])
+    for dim1 in range(1, ndim1 + 1):
+        for dim2 in range(1, ndim2 + 1):
+            markersum[dim1, dim2] = sum(concmatrix[:, dim1, dim2])
+
+    # Normalisation
+    normalised = np.zeros([M, ndim1 + 2, ndim2 + 2])
+    for m in range(M):
+        for m in range(M):
+            for dim1 in range(1, ndim1 + 1):
+                for dim2 in range(1, ndim2 + 1):
+                    normalised[m, dim1, dim2] = concmatrix[m, dim1, dim2] / markersum[dim1, dim2]
+                    if normalised[m, dim1, dim2] < E:
+                        normalised[m, dim1, dim2] = 0
+
+    return normalised
+
+
+#normalisedCpm = normalise(Cpm)
+
+
+#################### CONNECTIONS ######################
+
+# INITIAL CONNECTIONS
+
+def initialconections():
+    initialstrength = W / n0
+
+    for dim1 in range(1, NRdim1+1):
+        for dim2 in range(1, NRdim2+1):
+            if int(dim1 * ((NTdim1 - NL) / NRdim1) + NL) <= nTdim1:
+                if int(dim2 * ((NTdim2 - NL) / NRdim2) + NL) <= nTdim2:
+                    arrangement = np.zeros([NL * NL])
+                    arrangement[0:n0] = initialstrength
+                    random.shuffle(arrangement)
+                    arrangement = np.reshape(arrangement, (NL, NL))
+                    Wpt[int(dim1 * ((NTdim1 - NL) / NRdim1)) + 1: int(dim1 * ((NTdim1 - NL) / NRdim1) + NL) + 1,
+                        int(dim2 * ((NTdim2 - NL) / NRdim2)) + 1: int(dim2 * ((NTdim2 - NL) / NRdim2) + NL) + 1, dim1,
+                        dim2] = arrangement
+                else:
+                    arrangement = np.zeros([(NTdim2 - int(dim2 * ((NTdim2 - NL) / NRdim2)))*NL])
+                    arrangement[0:n0] = initialstrength
+                    random.shuffle(arrangement)
+                    arrangement = np.reshape(arrangement, (NL, NTdim2 - int(dim2 * ((NTdim2 - NL) / NRdim2))))
+                    Wpt[int(dim1 * ((NTdim1 - NL) / NRdim1)) + 1: int(dim1 * ((NTdim1 - NL) / NRdim1) + NL) + 1,
+                        int(dim2 * ((NTdim2 - NL) / NRdim2)) + 1: NTdim2 + 1, dim1,
+                        dim2] = arrangement
+            else:
+                arrangement = np.zeros([(NTdim1 - int(dim1 * ((NTdim1 - NL) / NRdim1))) * NL])
+                arrangement[0:n0] = initialstrength
+                random.shuffle(arrangement)
+                arrangement = np.reshape(arrangement, (NTdim1 - int(dim1 * ((NTdim1 - NL) / NRdim1)),NL))
+                Wpt[int(dim1 * ((NTdim1 - NL) / NRdim1)) + 1: NTdim1 + 1,
+                    int(dim2 * ((NTdim2 - NL) / NRdim2)) + 1: int(dim2 * ((NTdim2 - NL) / NRdim2) + NL) + 1, dim1,
+                    dim2] = arrangement
+
+initialconections()
+
+# INITIAL CONCENTRATIONS
+#Qtm = np.dot(Wpt, normalisedCpm)
+for t in range(td):
+    deltaconc = conc_change(Ctm, 'tectal')
+    Ctm += (deltaconc * deltat)
+normalisedCtm = normalise(Ctm)
+
+
+
+# ITERATIONS
+
+
+
 ##################### PLOT #######################
 params = {'font.size': '10'}
 plt.rcParams.update(params)
 
 for m in range(1, M + 1):
     plt.subplot(3, 2, m)
-    plt.pcolormesh(Cpm[m - 1, 1:NRdim1+1, 1:NRdim2+1])
+    plt.pcolormesh(Cpm[m - 1, 1:NRdim1 + 1, 1:NRdim2 + 1])
     plt.ylabel('Dimension 1')
     plt.xlabel('Dimension 2')
 
