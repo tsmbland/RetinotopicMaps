e4a2938e066e0bbdd085e62a8f721f74a3bae096
tsmbland <tb503@cam.ac.uk>
2017-02-11 23:06:57 +0000

    tidier code + various fixes

--- 2006 Model/2006 Model.py
+++ 2006 Model/2006 Model.py
@@ -8,10 +8,10 @@ start = time.time()
 
 # General
 Iterations = 100  # number of weight iterations
-NRdim1 = 10  # initial number of retinal cells
-NRdim2 = 10
-NTdim1 = 10  # initial number of tectal cells
-NTdim2 = 10
+NRdim1 = 20  # initial number of retinal cells
+NRdim2 = 20
+NTdim1 = 20  # initial number of tectal cells
+NTdim2 = 20
 
 # Retinal Gradients
 y0Rdim1 = 0.5  # conc in cell 0
@@ -36,25 +36,24 @@ deltatc = 0.1  # deltaC time step
 tc = 1  # concentration iterations per iteration
 
 # Synaptic modification
-W = 1  # total strength available to each presynaptic fibre
 gamma = 0.1
 kappa = 0.72
 deltatw = 0.1  # deltaW time step
 tw = 1  # weight iterations per iteration
 
 ################### VARIABLES ###################
-rmindim1 = 1
-rmaxdim1 = NRdim1
-rmindim2 = 1
-rmaxdim2 = NRdim2
-tmindim1 = 1
-tmaxdim1 = NTdim1
-tmindim2 = 1
-tmaxdim2 = NTdim2
-
+Rmindim1 = 1
+Rmaxdim1 = NRdim1
+Rmindim2 = 1
+Rmaxdim2 = NRdim2
+Tmindim1 = 1
+Tmaxdim1 = NTdim1
+Tmindim2 = 1
+Tmaxdim2 = NTdim2
 
 Wpt = np.zeros([Iterations + 1, NTdim1 + 2, NTdim2 + 2, NRdim1 + 2, NRdim2 + 2])  # synaptic strength matrix
 Spt = np.zeros([NTdim1 + 2, NTdim2 + 2, NRdim1 + 2, NRdim2 + 2])  # similarity
+Dpt = np.zeros([NTdim1 + 2, NTdim2 + 2, NRdim1 + 2, NRdim2 + 2])  # distance
 Cra = np.zeros([NRdim1 + 2, NRdim2 + 2])
 Crb = np.zeros([NRdim1 + 2, NRdim2 + 2])  # concentration of EphrinA/B in a retinal cell
 Cta = np.zeros([Iterations + 1, NTdim1 + 2, NTdim2 + 2])
@@ -67,10 +66,10 @@ Currentiteration = 0
 ####################### FUNCTIONS ###################
 
 def initialconnections():
-    for rdim1 in range(rmindim1, rmaxdim1 + 1):
-        for rdim2 in range(rmindim2, rmaxdim2 + 1):
-            for tdim1 in range(tmindim1, tmaxdim1 + 1):
-                for tdim2 in range(tmindim2, tmaxdim2 + 1):
+    for rdim1 in range(Rmindim1, Rmaxdim1 + 1):
+        for rdim2 in range(Rmindim2, Rmaxdim2 + 1):
+            for tdim1 in range(Tmindim1, Tmaxdim1 + 1):
+                for tdim2 in range(Tmindim2, Tmaxdim2 + 1):
                     Wpt[0, tdim1, tdim2, rdim1, rdim2] = np.random.uniform(0, 0.0001)
 
 
@@ -117,11 +116,11 @@ def setTectalGradients():
 def updateNct():
     # Neuron map
     nm = np.zeros([NTdim1 + 2, NTdim2 + 2])
-    nm[tmindim1:tmaxdim1 + 1, tmindim2:tmaxdim2 + 1] = 1
+    nm[Tmindim1:Tmaxdim1 + 1, Tmindim2:Tmaxdim2 + 1] = 1
 
     # Neighbour Count
-    for dim1 in range(tmindim1, tmaxdim1 + 1):
-        for dim2 in range(tmindim2, tmaxdim2 + 1):
+    for dim1 in range(Tmindim1, Tmaxdim1 + 1):
+        for dim2 in range(Tmindim2, Tmaxdim2 + 1):
             Nct[dim1, dim2] = nm[dim1 + 1, dim2] + nm[dim1 - 1, dim2] + nm[dim1, dim2 + 1] + nm[dim1, dim2 - 1]
 
 
@@ -130,8 +129,8 @@ def updateI():
     Itb[:, :] = 0
     wtotal = np.zeros([NTdim1 + 2, NTdim2 + 2])
 
-    for tdim1 in range(tmindim1, tmaxdim1 + 1):
-        for tdim2 in range(tmindim2, tmaxdim2 + 1):
+    for tdim1 in range(Tmindim1, Tmaxdim1 + 1):
+        for tdim2 in range(Tmindim2, Tmaxdim2 + 1):
             wtotal[tdim1, tdim2] = sum(sum(Wpt[Currentiteration, tdim1, tdim2, :, :]))
             Ita[tdim1, tdim2] = sum(sum(Wpt[Currentiteration, tdim1, tdim2, :, :] * Cra[:, :])) / wtotal[tdim1, tdim2]
             Itb[tdim1, tdim2] = sum(sum(Wpt[Currentiteration, tdim1, tdim2, :, :] * Crb[:, :])) / wtotal[tdim1, tdim2]
@@ -141,8 +140,8 @@ def updateCta():
     if Currentiteration > 0:
         Cta[Currentiteration, :, :] = Cta[Currentiteration - 1, :, :]
     for t in range(tc):
-        for tdim1 in range(tmindim1, tmaxdim1 + 1):
-            for tdim2 in range(tmindim2, tmaxdim2 + 1):
+        for tdim1 in range(Tmindim1, Tmaxdim1 + 1):
+            for tdim2 in range(Tmindim2, Tmaxdim2 + 1):
                 Cta[Currentiteration, tdim1, tdim2] += (alpha * (
                 1 - Ita[tdim1, tdim2] * Cta[Currentiteration, tdim1, tdim2])) * deltatc
 
@@ -151,25 +150,29 @@ def updateCtb():
     if Currentiteration > 0:
         Ctb[Currentiteration, :, :] = Ctb[Currentiteration - 1, :, :]
     for t in range(tc):
-        for tdim1 in range(tmindim1, tmaxdim1 + 1):
-            for tdim2 in range(tmindim2, tmaxdim2 + 1):
+        for tdim1 in range(Tmindim1, Tmaxdim1 + 1):
+            for tdim2 in range(Tmindim2, Tmaxdim2 + 1):
                 Ctb[Currentiteration, tdim1, tdim2] += (alpha * (
                 Itb[tdim1, tdim2] - Ctb[Currentiteration, tdim1, tdim2])) * deltatc
 
 
-def updateSpt():
-    dist = np.zeros([NTdim1 + 2, NTdim2 + 2, NRdim1 + 2, NRdim2 + 2])
-    for rdim1 in range(rmindim1, rmaxdim1 + 1):
-        for rdim2 in range(rmindim2, rmaxdim2 + 1):
-            for tdim1 in range(tmindim1, tmaxdim1 + 1):
-                for tdim2 in range(tmindim2, tmaxdim2 + 1):
-                    # Calculate distance
-                    dist[tdim1, tdim2, rdim1, rdim2] = ((Cra[rdim1, rdim2] * Cta[
+def updateDpt():
+    for rdim1 in range(Rmindim1, Rmaxdim1 + 1):
+        for rdim2 in range(Rmindim2, Rmaxdim2 + 1):
+            for tdim1 in range(Tmindim1, Tmaxdim1 + 1):
+                for tdim2 in range(Tmindim2, Tmaxdim2 + 1):
+                    Dpt[tdim1, tdim2, rdim1, rdim2] = ((Cra[rdim1, rdim2] * Cta[
                         Currentiteration, tdim1, tdim2] - 1) ** 2) + (
-                                                       (Crb[rdim1, rdim2] - Ctb[Currentiteration, tdim1, tdim2]) ** 2)
+                                                           (Crb[rdim1, rdim2] - Ctb[
+                                                               Currentiteration, tdim1, tdim2]) ** 2)
 
-                    # Calculate similarity
-                    Spt[tdim1, tdim2, rdim1, rdim2] = np.exp(-dist[tdim1, tdim2, rdim1, rdim2] / (2 * kappa ** 2))
+
+def updateSpt():
+    for rdim1 in range(Rmindim1, Rmaxdim1 + 1):
+        for rdim2 in range(Rmindim2, Rmaxdim2 + 1):
+            for tdim1 in range(Tmindim1, Tmaxdim1 + 1):
+                for tdim2 in range(Tmindim2, Tmaxdim2 + 1):
+                    Spt[tdim1, tdim2, rdim1, rdim2] = np.exp(-Dpt[tdim1, tdim2, rdim1, rdim2] / (2 * kappa ** 2))
 
 
 def updateWpt():
@@ -177,8 +180,8 @@ def updateWpt():
     for t in range(tw):
         numerator = Wpt[Currentiteration - 1, :, :, :, :] + deltatw * gamma * Spt
         denominator = np.zeros([NRdim1 + 2, NRdim2 + 2])
-        for rdim1 in range(rmindim1, rmaxdim1 + 1):
-            for rdim2 in range(rmindim2, rmaxdim2 + 1):
+        for rdim1 in range(Rmindim1, Rmaxdim1 + 1):
+            for rdim2 in range(Rmindim2, Rmaxdim2 + 1):
                 denominator[rdim1, rdim2] = sum(
                     sum((Wpt[Currentiteration - 1, :, :, rdim1, rdim2] + deltatw * gamma * Spt[
                                                                                            :, :,
@@ -208,6 +211,7 @@ updateCtb()
 for iteration in range(Iterations):
     Currentiteration += 1
 
+    updateDpt()
     updateSpt()
     updateWpt()
 
