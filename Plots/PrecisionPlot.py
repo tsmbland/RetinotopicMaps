78eed5c4bf1fe4cab8ff85b889a7751590912ecc
tsmbland <tb503@cam.ac.uk>
2017-02-20 22:31:59 +0000

    removed t=0 data from plots

--- Plots/PrecisionPlot.py
+++ Plots/PrecisionPlot.py
@@ -14,21 +14,24 @@ TRin = 10  # Temporal resolution of input files
 ######################## PLOTS #######################
 plt.subplot(1, 3, 1)
 plt.title('Receptive Field Separation')
-plt.plot(range(0, len(Fieldseparation) * TRin, TRin), Fieldseparation)
+plt.plot(range(TRin, len(Fieldseparation) * TRin, TRin), Fieldseparation[1:])
 plt.ylabel('Mean Receptive Field Separation')
 plt.xlabel('Time')
+plt.xlim(0, len(Fieldseparation) * TRin)
 
 plt.subplot(1, 3, 2)
 plt.title('Receptive Field Size')
-plt.plot(range(0, len(Fieldsize) * TRin, TRin), Fieldsize)
+plt.plot(range(TRin, len(Fieldsize) * TRin, TRin), Fieldsize[1:])
 plt.ylabel('Mean Receptive Field Diameter')
 plt.xlabel('Time')
+plt.xlim(0, len(Fieldsize) * TRin)
 
 plt.subplot(1, 3, 3)
 plt.title('Systems Match')
-plt.plot(range(0, len(Fieldsize) * TRin, TRin), Systemsmatch)
+plt.plot(range(TRin, len(Systemsmatch) * TRin, TRin), Systemsmatch[1:])
 plt.ylabel('Mean Distance Between Field Centre and Expected Field Centre')
 plt.xlabel('Time')
+plt.xlim(0, len(Systemsmatch) * TRin)
 
 ###################### END ########################
 params = {'font.size': '10'}
