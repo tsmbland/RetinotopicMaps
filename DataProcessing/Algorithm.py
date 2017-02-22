da69fadd6d111d416f23b48f2b1cacbc90358c98
tsmbland <tb503@cam.ac.uk>
2017-02-22 18:37:17 +0000

    combined field centres and map precision scripts into single file

--- DataProcessing/Algorithm.py
+++ DataProcessing/Algorithm.py
@@ -4,15 +4,22 @@ import time
 import Functions as f
 start = time.time()
 
-###################### FIELD CENTRES ######################
+
+##################### ALGORITHM #######################
 for i in range(0, len(f.Weightmatrix[:, 0, 0, 0, 0]), f.TRout // f.TRin):
     f.field_centre(i)
+    f.field_separation(i)
+    f.field_size(i)
+    f.systems_match(i)
     sys.stdout.write('\r%i percent' % (i * 100 / len(f.Weightmatrix[:, 0, 0, 0, 0])))
     sys.stdout.flush()
 
-##################### EXPORT DATA ###################
+##################### EXPORT DATA #####################
 
 np.save('../../RetinotopicMapsData/%s/FieldCentres' % ('{0:04}'.format(f.JobID)), f.FieldCentres)
+np.save('../../RetinotopicMapsData/%s/FieldSize' % ('{0:04}'.format(f.JobID)), f.FieldSize)
+np.save('../../RetinotopicMapsData/%s/FieldSeparation' % ('{0:04}'.format(f.JobID)), f.FieldSeparation)
+np.save('../../RetinotopicMapsData/%s/SystemsMatch' % ('{0:04}'.format(f.JobID)), f.SystemsMatch)
 np.save('../../RetinotopicMapsData/%s/SecondaryTR' % ('{0:04}'.format(f.JobID)), f.TRout)
 
 ###################### END ########################
