828d8b5c1d02305bf8209785fe527f15cf713aa2
tsmbland <tb503@cam.ac.uk>
2017-03-04 17:59:55 +0000

    added script to show stable precision measures with options for stability consditions

--- Plots/PrecisionChangeMeasures.py
+++ Plots/PrecisionChangeMeasures.py
@@ -5,9 +5,9 @@ import numpy as np
 
 JobID = int(input('JobID: '))
 
-Fieldsize = np.load('../../RetinotopicMapsData/%s/FieldSizeChange.npy' % ('{0:04}'.format(JobID)))
-Fieldseparation = np.load('../../RetinotopicMapsData/%s/FieldSeparationChange.npy' % ('{0:04}'.format(JobID)))
-Systemsmatch = np.load('../../RetinotopicMapsData/%s/SystemsMatchChange.npy' % ('{0:04}'.format(JobID)))
+Fieldsizechange = np.load('../../RetinotopicMapsData/%s/FieldSizeChange.npy' % ('{0:04}'.format(JobID)))
+Fieldseparationchange = np.load('../../RetinotopicMapsData/%s/FieldSeparationChange.npy' % ('{0:04}'.format(JobID)))
+Systemsmatchchange = np.load('../../RetinotopicMapsData/%s/SystemsMatchChange.npy' % ('{0:04}'.format(JobID)))
 TRin = np.load('../../RetinotopicMapsData/%s/SecondaryTR.npy' % ('{0:04}'.format(JobID)))
 
 Iteration = int(input('Iteration: '))
@@ -16,6 +16,6 @@ Iteration = int(input('Iteration: '))
 
 #################### PRINT DATA #######################
 
-print('Field Separation: ', Fieldseparation[int(Iteration/TRin)])
-print('Field Size: ', Fieldsize[int(Iteration/TRin)])
-print('Systems Match: ', Systemsmatch[int(Iteration/TRin)])
+print('Field Separation Change: ', Fieldseparationchange[int(Iteration/TRin)])
+print('Field Size Change: ', Fieldsizechange[int(Iteration/TRin)])
+print('Systems Match Change: ', Systemsmatchchange[int(Iteration/TRin)])
