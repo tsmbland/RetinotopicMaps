42613a1bd9f3d55c437d9933c9f2fbcc6c3b90d2
tsmbland <tb503@cam.ac.uk>
2017-04-11 21:32:50 +0100

    changes to plot

--- ReportFigs/SurgeryPlot4.py
+++ ReportFigs/SurgeryPlot4.py
@@ -70,30 +70,30 @@ TRin3 = np.load('../../RetinotopicMapsData/%s/SecondaryTR.npy' % ('{0:04}'.forma
 
 # Separation Plot
 ax = fig.add_subplot(2, 3, 4)
-ax.plot(range(TRin1, len(Fieldseparation1) * TRin1, TRin1), Fieldseparation1[1:],
-        range(TRin2, len(Fieldseparation2) * TRin2, TRin2), Fieldseparation2[1:],
-        range(TRin3, len(Fieldseparation3) * TRin3, TRin3), Fieldseparation3[1:], c=['b', 'r', 'g'])
-plt.hline(0.75, c='b')
-plt.hline(1.5, c='r')
-plt.hline(1, c='g')
+ax.plot(range(TRin1, len(Fieldseparation1) * TRin1, TRin1), Fieldseparation1[1:], c='b')
+ax.plot(range(TRin2, len(Fieldseparation2) * TRin2, TRin2), Fieldseparation2[1:], c='r')
+ax.plot(range(TRin3, len(Fieldseparation3) * TRin3, TRin3), Fieldseparation3[1:], c='g')
+plt.axhline(0.75, c='b', ls='--', lw='0.5')
+plt.axhline(1.5, c='r', ls='--', lw='0.5')
+plt.axhline(1, c='g', ls='--', lw='0.5')
 ax.set_ylabel('Mean Receptive Field Separation')
 ax.set_xlabel('Iterations')
 ax.set_title('D')
 
 # Size Plot
 ax = fig.add_subplot(2, 3, 5)
-ax.plot(range(TRin1, len(Fieldsize1) * TRin1, TRin1), Fieldsize1[1:],
-        range(TRin2, len(Fieldsize2) * TRin2, TRin2), Fieldsize2[1:],
-        range(TRin3, len(Fieldsize3) * TRin3, TRin3), Fieldsize3[1:])
+ax.plot(range(TRin1, len(Fieldsize1) * TRin1, TRin1), Fieldsize1[1:], c='b')
+ax.plot(range(TRin2, len(Fieldsize2) * TRin2, TRin2), Fieldsize2[1:], c='r')
+ax.plot(range(TRin3, len(Fieldsize3) * TRin3, TRin3), Fieldsize3[1:], c='g')
 ax.set_ylabel('Mean Receptive Field Size')
 ax.set_xlabel('Iterations')
 ax.set_title('E')
 
 # Systems Match Plot
 ax = fig.add_subplot(2, 3, 6)
-ax.plot(range(TRin1, len(Systemsmatch1) * TRin1, TRin1), Systemsmatch1[1:],
-        range(TRin2, len(Systemsmatch2) * TRin2, TRin2), Systemsmatch2[1:],
-        range(TRin3, len(Systemsmatch3) * TRin3, TRin3), Systemsmatch3[1:])
+ax.plot(range(TRin1, len(Systemsmatch1) * TRin1, TRin1), Systemsmatch1[1:], c='b')
+ax.plot(range(TRin2, len(Systemsmatch2) * TRin2, TRin2), Systemsmatch2[1:], c='r')
+ax.plot(range(TRin3, len(Systemsmatch3) * TRin3, TRin3), Systemsmatch3[1:], c='g')
 ax.set_ylabel('Systems Match Score')
 ax.set_xlabel('Iterations')
 ax.set_title('F')
