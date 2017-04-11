6685b4901f4a6b9c48aa81edf8a2f65599539e9e
tsmbland <tb503@cam.ac.uk>
2017-04-11 18:04:12 +0100

    added new figure

--- ReportFigs/SuperPrecisionPlot.py
+++ ReportFigs/SuperPrecisionPlot.py
@@ -7,7 +7,7 @@ plt.rcParams['savefig.dpi'] = 600
 ##################### IMPORT DATA #####################
 
 JobID = 817
-EB = 'y' #input('Exclude Boundaries? (y/n) ')
+EB = 'y'  # input('Exclude Boundaries? (y/n) ')
 
 if EB == 'n':
     Fieldsize = np.load('../../RetinotopicMapsData/%s/FieldSize.npy' % ('{0:04}'.format(JobID)))
@@ -36,23 +36,25 @@ TRin = np.load('../../RetinotopicMapsData/%s/SecondaryTR.npy' % ('{0:04}'.format
 ######################## PLOTS #######################
 ax1 = plt.subplot2grid((2, 2), (0, 0))
 ax1.set_title('A', x=0)
-ax1.plot(range(TRin, len(Fieldseparation) * TRin, TRin), Fieldseparation[1:])
+ax1.plot(range(TRin, len(Fieldseparation) * TRin, TRin), Fieldseparation[1:], c='k')
 ax1.set_ylabel('Mean Receptive Field Separation')
 ax1.set_xlabel('Iterations')
 ax1.set_xlim(0, 5000)
+ax1.set_ylim(0, 3)
 
 ax2 = plt.subplot2grid((2, 2), (0, 1))
 ax2.set_title('B', x=0)
-ax2.plot(range(TRin, len(Fieldsize) * TRin, TRin), Fieldsize[1:])
+ax2.plot(range(TRin, len(Fieldsize) * TRin, TRin), Fieldsize[1:], c='k')
 ax2.set_ylabel('Mean Receptive Field Diameter')
 ax2.set_xlabel('Iterations')
 ax2.set_xlim(0, 5000)
 
 ax3 = plt.subplot2grid((2, 2), (1, 0), colspan=2)
 ax3.set_title('C', x=0)
-ax3.plot(range(TRin, len(Systemsmatch) * TRin, TRin), Systemsmatch[1:])
-ax3.set_ylabel('Systems Match')
+ax3.plot(range(TRin, len(Systemsmatch) * TRin, TRin), Systemsmatch[1:], c='k')
+ax3.set_ylabel('Systems Match Score')
 ax3.set_xlabel('Iterations')
 ax3.set_xlim(0, 2000)
+ax3.set_ylim(0, 5)
 
 plt.show()
